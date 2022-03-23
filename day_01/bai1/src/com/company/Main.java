7723de970dbb3ddbcb073f2bead0960bda29b42e
Dattm-coder <55756383+Dattm-coder@users.noreply.github.com>
2022-03-23 12:36:49 +0700

    update

---
+++ day_01/bai1/src/com/company/Main.java
@@ -0,0 +1,71 @@
+package com.company;
+
+public class Main {
+    public int age;
+    public static final double PI = 3.14;
+    public static void main(String[] args) {
+//        System.out.println("hello, mọi ngừi");
+//
+//        int age;//khai báo biến
+//        age = 26;
+//        System.out.println(age);
+//        long salary = 1000000L;
+//        System.out.println("lương của tôi:"+salary);
+//
+//        float weight = 45.5f;
+//        double height = 1.6;
+//
+//        char c = 'c';
+//        char five = '5';
+//        char s = 65;
+//        System.out.println(five);
+//        System.out.println(s);
+//        double height = 1.5;
+//        double weight = 40;
+//
+//        double bmi = weight/(height*height);
+//        System.out.println("chỉ số bmi: "+bmi);
+
+//        double toan = 7.6;
+//        double li = 8.5;
+//        double hoa = 9.5;
+//
+//        double diemtb= (toan+li+hoa)/3;
+//        System.out.println("Diem trung bình: "+diemtb);
+
+
+//        System.out.println("PI: "+PI);
+//
+//        System.out.println("Xin \n chào \t mọi \"người\" ");
+    int a = 5;
+    int b = 2;
+        System.out.println(a%2==0 ? a + "là số chẵn" : a + "là số lẻ");
+    boolean check = a==b;
+    int age = 20;
+        System.out.println(age >=18 ? "bạn đủ đuổi" : "bạn còn non");
+//    a +=b; a = a+b
+
+        System.out.println(check);
+
+        System.out.println(a == b);
+
+        System.out.println(a != b);
+
+
+    double a1 = (double)a;
+
+    double c = (double)a/b;
+//    double c = 5/2;
+        System.out.println(c);
+
+        int c1 = (int)c;
+        System.out.println(c1);
+
+
+
+
+    }
+
+
+
+}

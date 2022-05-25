69b20e0b562d91621d7833f7b30c24aefbfd10ba
Dattm-coder <55756383+Dattm-coder@users.noreply.github.com>
2022-05-25 14:32:23 +0700

    update

---
+++ day_12/Collection_demo/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Student {
+    public static void main(String[] args) {
+        ArrayList<Student> list = new ArrayList<>();
+        list.add(new Student(1,"Quang","10a",9));
+        list.add(new Student(2,"Huy","10a",9));
+        list.add(new Student(3,"Tuấn","10a",9));
+        list.add(new Student(4,"Linh","10a",9));
+        list.add(new Student(5,"Thảo","10a",9));
+
+    }
+}
